a1c23ac1701cd10017b3bb3f930e67c5dcd0a9c6
FrancisB1 <gaspump1112@gmail.com>
2015-09-17 11:34:09 -0400

    Added BurmaShave

--- ChapterOne/BurmaShave/Program.cs
+++ ChapterOne/BurmaShave/Program.cs
@@ -9,6 +9,12 @@ namespace BurmaShave
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Men");
+            Console.WriteLine("with whiskers");
+            Console.WriteLine("'Neath their noses");
+            Console.WriteLine("Oughta have to kiss");
+            Console.WriteLine("like eskimoses");
+            Console.WriteLine("BurmaShave");
         }
     }
 }
