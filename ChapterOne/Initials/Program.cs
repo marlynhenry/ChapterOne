d8587407a44019d5fe6c753dba394c09388ac622
FrancisB1 <gaspump1112@gmail.com>
2015-09-17 11:19:22 -0400

    ADDED INITIALS

--- ChapterOne/Initials/Program.cs
+++ ChapterOne/Initials/Program.cs
@@ -9,6 +9,12 @@ namespace Initials
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("M    HHHHH");
+            Console.WriteLine("M M HHH");
+            Console.WriteLine("MMMM HHH");
+            Console.WriteLine("M M M M HHH");
+            Console.WriteLine("MM HHHH");
+            Console.WriteLine("M M M M M HHHHHH");
         }
     }
 }
